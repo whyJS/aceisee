e92f86f2118e5dbd1484188f20bb2db733338737
why <why3935308@163.com>
2020-05-26 14:58:14 +0800

    xx2

--- src/router/page/index.js
+++ src/router/page/index.js
@@ -70,6 +70,41 @@ export default [
     ]
   },
   {
+    path: "/wel",
+    component: Layout,
+    name: "zz",
+    source: "el-icon-s-grid",
+    redirect: "/wel/index",
+    children: [
+      {
+        path: "/wel/index",
+        name: "首页",
+        meta: {
+          i18n: "dashboard"
+        },
+        component: () =>
+          import(/* webpackChunkName: "views" */ "@/views/wel/index")
+      }
+    ]
+  },
+
+  {
+    path: "/info",
+    component: Layout,
+    redirect: "/info/index",
+    children: [
+      {
+        path: "/info/index",
+        name: "个人信息",
+        meta: {
+          i18n: "info"
+        },
+        component: () =>
+          import(/* webpackChunkName: "views" */ "@/views/system/userinfo")
+      }
+    ]
+  },
+  {
     path: "*",
     redirect: "/404"
   }
--- src/router/views/index.js
+++ src/router/views/index.js
@@ -1,38 +1,122 @@
 import Layout from "@/page/index/";
 
 export default [
+  // TOEFL
   {
-    path: "/wel",
+    path: "/toefl",
     component: Layout,
-    name: "zz",
-    source:"el-icon-s-grid",
-    redirect: "/wel/index",
+    redirect: "/toefl/learning",
+    name: "TOEFL",
+    source: "el-icon-s-grid",
+    meta: {},
     children: [
       {
-        path: "/wel/index",
-        name: "首页",
-        meta: {
-          i18n: "dashboard"
-        },
-        component: () =>
-          import(/* webpackChunkName: "views" */ "@/views/wel/index")
+        path: "/toefl/learning",
+        component: () => import("@/views/toefl/learning.vue"),
+        name: "Learning",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/toefl/exercise",
+        component: () => import("@/views/toefl/exercise.vue"),
+        name: "Exercise",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/toefl/test",
+        component: () => import("@/views/toefl/test.vue"),
+        name: "Test",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/toefl/wordbook",
+        component: () => import("@/views/toefl/wordbook.vue"),
+        name: "Word Book",
+        source: "el-icon-s-grid",
+        meta: {}
       }
     ]
   },
 
+  // SAT
   {
-    path: "/info",
+    path: "/sat",
     component: Layout,
-    redirect: "/info/index",
+    redirect: "/sat/learning",
+    name: "SAT",
+    source: "el-icon-s-grid",
+    meta: {},
     children: [
       {
-        path: "/info/index",
-        name: "个人信息",
-        meta: {
-          i18n: "info"
-        },
-        component: () =>
-          import(/* webpackChunkName: "views" */ "@/views/system/userinfo")
+        path: "/sat/learning",
+        component: () => import("@/views/sat/learning.vue"),
+        name: "Learning",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/sat/exercise",
+        component: () => import("@/views/sat/exercise.vue"),
+        name: "Exercise",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/sat/test",
+        component: () => import("@/views/sat/test.vue"),
+        name: "Test",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/sat/wordbook",
+        component: () => import("@/views/sat/wordbook.vue"),
+        name: "Word Book",
+        source: "el-icon-s-grid",
+        meta: {}
+      }
+    ]
+  },
+
+  // SSAT
+  {
+    path: "/ssat",
+    component: Layout,
+    redirect: "/ssat/learning",
+    name: "SSAT",
+    source: "el-icon-s-grid",
+    meta: {},
+    children: [
+      {
+        path: "/ssat/learning",
+        component: () => import("@/views/ssat/learning.vue"),
+        name: "Learning",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/ssat/exercise",
+        component: () => import("@/views/ssat/exercise.vue"),
+        name: "Exercise",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/ssat/test",
+        component: () => import("@/views/ssat/test.vue"),
+        name: "Test",
+        source: "el-icon-s-grid",
+        meta: {}
+      },
+      {
+        path: "/ssat/wordbook",
+        component: () => import("@/views/ssat/wordbook.vue"),
+        name: "Word Book",
+        source: "el-icon-s-grid",
+        meta: {}
       }
     ]
   }
