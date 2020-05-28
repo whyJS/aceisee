f5b179166e7946fc2442ac09531822bc6d29ca60
why <124258920@qq.com>
2020-05-28 23:48:06 +0800

    sss

--- src/views/study/learning.vue
+++ src/views/study/learning.vue
@@ -23,9 +23,9 @@
                     border
                     lazy
                     align="left">
-                    <el-table-column slot-scope="scope" align="center">
-                      <template>
-                        <span>1</span>
+                    <el-table-column  align="center">
+                      <template slot-scope="scope">
+                        <div class="leftItem">{{scope.row.text}}</div>
                       </template>
                     </el-table-column>
                   </el-table>
@@ -44,8 +44,20 @@
             <el-table-column align="center"
                              label="Detail">
               <template>
-                <div class="listLeft">
-                  
+                <div class="listRight">
+                  <el-table :data="listLeft"
+                    ref="crud"
+                    highlight-current-row
+                   @row-click = "btn"
+                    border
+                    lazy
+                    align="left">
+                    <el-table-column  align="center">
+                      <template slot-scope="scope">
+                        <div class="leftItem">{{scope.row.text}}</div>
+                      </template>
+                    </el-table-column>
+                  </el-table>
                 </div>
               </template>
             </el-table-column>
@@ -69,7 +81,31 @@ export default {
         fontWeight: 700,
         background: '#f5f5f5'
       },
-      listLeft:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26],
+      listLeft:[
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'1'},
+        {text:'2'},
+
+      ],
       styleCssLeft:false,
     };
   },
@@ -90,6 +126,18 @@ export default {
   box-sizing: border-box;
   height: calc(100vh - 400px);
 }
+
+.listLeft{
+  .leftItem{
+    width: 100%;
+    text-align: left;
+  }
+}
+.listRight{
+  height: calc(100vh - 500px);
+  overflow: auto;
+
+}
 </style>
 
 <style lang="scss">
@@ -99,12 +147,13 @@ export default {
   /* 用来设置当前页面element全局table 选中某行时的背景色*/
   .el-table__body tr.current-row>td{
     background-color: #409EFF !important;
-    /* color: #f19944; */  /* 设置文字颜色，可以选择不设置 */
+    color: #fff;
   }
   /* 用来设置当前页面element全局table 鼠标移入某行时的背景色*/
   .el-table--enable-row-hover .el-table__body tr:hover>td {
     background-color: #409EFF;
     /* color: #f19944; */ /* 设置文字颜色，可以选择不设置 */
+    color: #fff;
   }
 }
 
